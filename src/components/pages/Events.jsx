bad6545092a279f0a388b6de71eaa717bbccb83f
techfestriti <techfestriti@gmail.com>
2026-07-30 19:42:19 +0530

    Update Events.jsx

--- src/components/pages/Events.jsx
+++ src/components/pages/Events.jsx
@@ -6,12 +6,12 @@ const Events = () => {
 
   // Sample event images
   const brochureImages = [
-    { id: 1, src: '2.jpg', alt: 'Event Brochure 1' },
-    { id: 2, src: '3.jpg', alt: 'Event Brochure 2' },
-    { id: 3, src: '4.jpg', alt: 'Event Brochure 3' },
-    { id: 4, src: '5.jpg', alt: 'Event Brochure 4' },
-    { id: 5, src: '6.jpg', alt: 'Event Brochure 5' },
-    { id: 6, src: '7.jpg', alt: 'Event Brochure 6' },
+    { id: 1, src: '2.png', alt: 'Event Brochure 1' },
+    { id: 2, src: '3.png', alt: 'Event Brochure 2' },
+    { id: 3, src: '4.png', alt: 'Event Brochure 3' },
+    { id: 4, src: '5.png', alt: 'Event Brochure 4' },
+    { id: 5, src: '6.png', alt: 'Event Brochure 5' },
+    { id: 6, src: '7.png', alt: 'Event Brochure 6' },
   ];
 
   // Function to handle download
